9fdcfc05f35384b72529d75ccd87f422a58833d6
Bryan Lau <bryan.d.lau@outlook.com>
2016-04-17 22:46:47 -0700

    Use only MLab database for consistency

--- config.js
+++ config.js
@@ -2,9 +2,11 @@
 exports.tokenSecret = "Greg's secret is not helping me. ):";
 
 // default to a 'localhost' configuration:
-var connection_string = 'mongodb://goalbuddies:pwd123@ds038888.mongolab.com:38888/goalbuddies'
-    || '127.0.0.1:27017/v2';
+var connection_string =
+	'mongodb://goalbuddies:pwd123@ds038888.mongolab.com:38888/goalbuddies';
 // if OPENSHIFT env variables are present, use the available connection info:
+// Text search has to be enabled manually, don't use Openshift (Mongo v2.4)
+/*
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connection_string =
         process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
@@ -13,5 +15,6 @@ if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
 }
+*/
 
 exports.mongoAddr = connection_string;
